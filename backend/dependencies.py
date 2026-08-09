90a30bb1ce5c842f6bcc637527c3ab83d4d27f4d
shailesh sabade <shaileshsabade1@gmail.com>
2026-08-09 16:46:32 +0530

    dependencies.py

--- backend/dependencies.py
+++ backend/dependencies.py
@@ -1,6 +1,6 @@
 from fastapi import Depends, HTTPException
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
-from jose import JWTError, jwt
+from jose import jwt, JWTError
 from sqlalchemy.orm import Session
 
 from database import get_db
@@ -9,41 +9,62 @@ from models import User
 SECRET_KEY = "HealthForecastAISecretKey"
 ALGORITHM = "HS256"
 
-# Bearer Authentication
 security = HTTPBearer()
-
 
 def get_current_user(
     credentials: HTTPAuthorizationCredentials = Depends(security),
     db: Session = Depends(get_db)
 ):
+
     token = credentials.credentials
 
-    credentials_exception = HTTPException(
-        status_code=401,
-        detail="Could not validate credentials"
-    )
+    print("\n========== AUTH DEBUG ==========")
+    print("TOKEN:", token)
 
     try:
+
         payload = jwt.decode(
             token,
             SECRET_KEY,
             algorithms=[ALGORITHM]
         )
 
+        print("PAYLOAD:", payload)
+
         email = payload.get("sub")
 
+        print("EMAIL:", email)
+
         if email is None:
-            raise credentials_exception
+            print("❌ Email is None")
+            raise HTTPException(
+                status_code=401,
+                detail="Email missing in token"
+            )
 
-    except JWTError:
-        raise credentials_exception
+        user = db.query(User).filter(
+            User.email == email
+        ).first()
 
-    user = db.query(User).filter(
-        User.email == email
-    ).first()
+        print("USER:", user)
 
-    if user is None:
-        raise credentials_exception
+        if user is None:
+            print("❌ User not found")
+            raise HTTPException(
+                status_code=401,
+                detail="User not found"
+            )
 
-    return user
+        print("✅ Authentication Success")
+        print("==============================\n")
+
+        return user
+
+    except JWTError as e:
+
+        print("JWT ERROR:", e)
+
+        raise HTTPException(
+            status_code=401,
+            detail="Could not validate credentials"
+        )
