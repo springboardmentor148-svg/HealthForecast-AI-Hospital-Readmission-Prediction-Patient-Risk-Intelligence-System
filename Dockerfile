04ccfe21e737ef22ceb297211497259f67dff4bc
Prathima Itikala <prathimaitikala@gmail.com>
2026-08-11 23:38:35 +0530

    Add persistent Docker database storage

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM python:3.13-slim
 
 WORKDIR /app
-
+RUN mkdir -p /app/data
 COPY requirements.txt .
 
 RUN pip install --no-cache-dir -r requirements.txt
--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # This creates a simple database file called healthforecast.db
-DATABASE_URL = "sqlite:///./healthforecast.db"
+DATABASE_URL = "sqlite:///./data/healthforecast.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
