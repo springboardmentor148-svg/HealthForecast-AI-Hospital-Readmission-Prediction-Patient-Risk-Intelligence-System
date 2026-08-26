44c5702e42533ecaed6af34355352bfe15116606
uduthavyshnavi <uduthavyshnavi@gmail.com>
2026-08-26 18:27:49 +0530

    Updated backend routes and database configurations

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -39,7 +39,7 @@ async function apiRequest(endpoint, options = {}) {
   // ----------------------------------------------------------
 
   const response = await fetch(
-    `${API_URL}${endpoint}`,
+    `${API_BASE_URL}/login`,
     {
       ...options,
       headers,
